aea1b3ecded7c9e841e09efa2f66d15c4a230f16
wael <wael.tellat@gmail.com>
2020-02-25 23:20:47 +0100

    fix: remove console.log

--- index.js
+++ index.js
@@ -884,7 +884,6 @@ function Num2Word_DK() {
   this.ordflag = false
   this.cards = [{"1000000000000000000000000000": 'quadrillarder'}, {"1000000000000000000000000": 'quadrillioner'}, {"1000000000000000000000": 'trillarder'}, {"1000000000000000000": 'trillioner'}, {"1000000000000000": 'billarder'}, {"1000000000000": 'billioner'}, {"1000000000": 'millarder'}, {"1000000": 'millioner'}, {"1000": 'tusind'}, {"100": 'hundrede'}, {"90": 'halvfems'}, {"80": 'firs'}, {"70": 'halvfjerds'}, {"60": 'treds'}, {"50": 'halvtreds'}, {"40": 'fyrre'}, {"30": 'tredive'}, {"20": 'tyve'}, {"19": 'nitten'}, {"18": 'atten'}, {"17": 'sytten'}, {"16": 'seksten'}, {"15": 'femten'}, {"14": 'fjorten'}, {"13": 'tretten'}, {"12": 'tolv'}, {"11": 'elleve'}, {"10": 'ti'}, {"9": 'ni'}, {"8": 'otte'}, {"7": 'syv'}, {"6": 'seks'}, {"5": 'fem'}, {"4": 'fire'}, {"3": 'tre'}, {"2": 'to'}, {"1": 'et'}, {"0": 'nul'}]
   this.merge = (curr, next) => {
-    console.log(curr, next)
     var ctext = Object.keys(curr)[0], cnum = parseInt(Object.values(curr)[0])
     var ntext = Object.keys(next)[0], nnum = parseInt(Object.values(next)[0])
     var val = 1
@@ -911,7 +910,6 @@ function Num2Word_DK() {
       val = cnum + nnum
     }
     var word = ctext + ntext
-    console.log("Word: ", word, " val: ", val)
     return { [`${word}`]: val }
   }
 }
