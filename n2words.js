1d1324834f683997b6470bcbc2d12d6efa4ec6bd
wael <wael.tellat@gmail.com>
2020-03-04 00:29:18 +0100

    Add Korean (ko)

--- n2words.js
+++ n2words.js
@@ -5,7 +5,7 @@
 "use strict";
 exports = module.exports = n2words;
 
-var supportedLanguages = ['en', 'fr', 'es', 'de', 'pt', 'it', 'tr', 'ru', 'cz', 'no', 'dk', 'pl', 'uk', 'lt', 'lv', 'ar', 'he']
+var supportedLanguages = ['en', 'fr', 'es', 'de', 'pt', 'it', 'tr', 'ru', 'cz', 'no', 'dk', 'pl', 'uk', 'lt', 'lv', 'ar', 'he', 'ko']
 
 /**
  * Converts numbers to their written form.
@@ -59,6 +59,8 @@ function n2words(n, options) {
     num = new Num2Word_AR();
   } else if (lang === 'HE') {  // only for numbers <= 9999
     num = new Num2Word_HE();
+  } else if (lang === 'KO') {
+    num = new Num2Word_KO();
   }
   return num.toCardinal(n);
 }
@@ -142,9 +144,7 @@ function Num2Word_Base() {
 
 function Num2Word_EN() {
   Num2Word_Base.call(this);
-
   this.cards = [{"1000000000000000000000000000": 'octillion'}, {"1000000000000000000000000": 'septillion'}, {"1000000000000000000000": 'sextillion'}, {"1000000000000000000": 'quintillion'}, { "1000000000000000": 'quadrillion' }, { "1000000000000": 'trillion' }, { "1000000000": 'billion' }, { "1000000": 'million' }, { "1000": 'thousand' }, { "100": 'hundred' }, { "90": 'ninety' }, { "80": 'eighty' }, { "70": 'seventy' }, { "60": 'sixty' }, { "50": 'fifty' }, { "40": 'forty' }, { "30": 'thirty' }, { "20": 'twenty' }, { "19": 'nineteen' }, { "18": 'eighteen' }, { "17": 'seventeen' }, { "16": 'sixteen' }, { "15": 'fifteen' }, { "14": 'fourteen' }, { "13": 'thirteen' }, { "12": 'twelve' }, { "11": 'eleven' }, { "10": 'ten' }, { "9": 'nine' }, { "8": 'eight' }, { "7": 'seven' }, { "6": 'six' }, { "5": 'five' }, { "4": 'four' }, { "3": 'three' }, { "2": 'two' }, { "1": 'one' }, { "0": 'zero' }]
-
   this.merge = (lpair, rpair) => { //{'one':1}, {'hundred':100}
     var ltext = Object.keys(lpair)[0], lnum = parseInt(Object.values(lpair)[0])
     var rtext = Object.keys(rpair)[0], rnum = parseInt(Object.values(rpair)[0])
@@ -154,7 +154,6 @@ function Num2Word_EN() {
     else if (rnum > lnum) return { [`${ltext} ${rtext}`]: lnum * rnum }
     return { [`${ltext} ${rtext}`]: lnum + rnum }
   }
-
 }
 
 function Num2Word_FR() {
@@ -1299,6 +1298,19 @@ function Num2Word_HE() {
       words[words.length - 1] = this.AND + words[words.length - 1]
     }
     return words.join(' ')
+  }
+}
+
+function Num2Word_KO() {
+  Num2Word_Base.call(this);
+  this.cards = [{ '10000000000000000000000000000': '양' }, { '1000000000000000000000000': '자' }, { '100000000000000000000': '해' }, { '10000000000000000': '경' }, { '1000000000000': '조' }, { '100000000': '억' }, { '10000': '만' }, { '1000': '천' }, { '100': '백' }, { '10': '십' }, { '9': '구' }, { '8': '팔' }, { '7': '칠' }, { '6': '육' }, { '5': '오' }, { '4': '사' }, { '3': '삼' }, { '2': '이' }, { '1': '일' }, { '0': '영' }]
+  this.merge = (lpair, rpair) => {
+    var ltext = Object.keys(lpair)[0], lnum = parseInt(Object.values(lpair)[0])
+    var rtext = Object.keys(rpair)[0], rnum = parseInt(Object.values(rpair)[0])
+    if (lnum == 1 && rnum <= 10000) return { [rtext]: rnum }
+    else if (10000 > lnum && lnum > rnum) return { [`${ltext}${rtext}`]: lnum + rnum }
+    else if (lnum >= 10000 && lnum > rnum) return { [`${ltext} ${rtext}`]: lnum + rnum }
+    else return { [`${ltext}${rtext}`]: lnum * rnum }
   }
 }
 
