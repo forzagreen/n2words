f09f459c0ac7ac55b0249910a8bd7c6a1734baf7
Tyler Vigario <TylerVigario90@gmail.com>
2021-08-10 12:14:21 -0700

    Configs: update dependencies, linting, camelCase (#51)
    * Bump dependencies
    
    * Update ESLint config
    
    * Small corrections to JSDoc
    
    * Update WebPack config
    
    * Bump dev dependencies
    
    * Update dependencies
    
    * Update linting
    
    * Bump dev dependencies
    
    * Drop ESM & update GitHub actions
    
    * Fix linting issues (migrated to camelCase)
    
    * Commit package-lock.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 module.exports = {
   mode: 'production',
   entry: './lib/n2words.mjs',
@@ -8,13 +6,12 @@ module.exports = {
     libraryTarget: 'umd',
     libraryExport: 'default',
     filename: 'n2words.js',
-    path: path.resolve(__dirname, 'dist'),
     globalObject: 'this',
   },
   module: {
     rules: [{
       test: /\.m?js$/,
-      exclude: /(node_modules|bower_components)/,
+      exclude: /node_modules/,
       resolve: {
         fullySpecified: false,
       },
@@ -26,7 +23,7 @@ module.exports = {
               '@babel/preset-env',
               {
                 useBuiltIns: 'usage',
-                corejs: '3.6',
+                corejs: '3.16',
               },
             ],
           ],
