ef849432be93ab74c8175f17a142bf613aa0c706
Tyler Vigario <TylerVigario90@gmail.com>
2023-02-08 14:53:29 -0800

    Update dependencies (#74)
    * Bump dependencies
    
    * Bump CI Node versions
    
    * Build package
    
    * Update .github/workflows/nodejs.yml
    
    * Bump ava
    
    * Temporarily use npm install for GitHub Actions testing
    
    ---------
    
    Co-authored-by: Wael <wael.tellat@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
               '@babel/preset-env',
               {
                 useBuiltIns: 'usage',
-                corejs: '3.23.2',
+                corejs: '3.27.2',
               },
             ],
           ],
