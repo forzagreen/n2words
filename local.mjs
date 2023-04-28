acb816d62cd8cc949f3753ada2a3a2e590ca6c2c
Tyler Vigario <TylerVigario90@gmail.com>
2023-04-27 18:32:52 -0700

    Small cleanup

--- local.mjs
+++ local.mjs
@@ -1,3 +1,10 @@
+/* eslint-disable import/no-nodejs-modules */
 import n2words from './lib/i18n/EN.mjs';
+import * as readline from 'node:readline/promises';
+import {stdin, stdout} from 'node:process';
 
-console.log(n2words(10));
+const rl = readline.createInterface({stdin, stdout});
+
+console.log(n2words(await rl.question('Value to convert?')));
+
+rl.close();
