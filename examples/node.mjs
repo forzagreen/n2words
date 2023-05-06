6e856a72ee35b53834eb6579db84436bc3163eac
Tyler Vigario <TylerVigario90@gmail.com>
2023-05-05 19:16:42 -0700

    Make code readable (#85)
    * Document code for use with development tools
    * Improve local debugging for developers
    * Performance improvements

---
+++ examples/node.mjs
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-nodejs-modules */
+import n2words from '../lib/i18n/EN.mjs';
+import * as readline from 'node:readline/promises';
+import {stdin as input, stdout as output} from 'node:process';
+
+const rl = readline.createInterface({input, output});
+
+console.log(n2words(await rl.question('Value to convert? ')));
+
+rl.close();
