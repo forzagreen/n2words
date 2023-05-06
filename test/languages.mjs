e1f998b2fc9806707c9c9da771a1e8acd92c833a
Tyler Vigario <TylerVigario90@gmail.com>
2023-05-06 13:48:44 -0700

    Ability to run language/i18n tests separately

--- test/languages.mjs
+++ test/languages.mjs
@@ -61,79 +61,24 @@ const i18n = {
   zh: ZH,
 };
 
-Object.keys(i18n).forEach(language => {
+const parameter = process.argv[2];
+const value = process.argv[3];
+
+if (parameter == '--language' || parameter == '--lang') {
+  testLanguage(value);
+} else {
+  Object.keys(i18n).forEach(language => {
+    testLanguage(language);
+  });
+}
+
+function testLanguage(language) {
   test(language, t => {
     i18n[language].forEach(problem => {
       t.is(
-        n2words(problem[0], Object.assign({ lang: language }, problem[2])),
+        n2words(problem[0], Object.assign({lang: language}, problem[2])),
         problem[1]
       );
     });
   });
-});
-
-test('english is default', t => {
-  t.is(n2words(12), 'twelve');
-  t.is(n2words(356), 'three hundred and fifty-six');
-});
-
-test('accept valid string numbers', t => {
-  t.is(n2words('12'), 'twelve');
-  t.is(n2words('0012'), 'twelve');
-  t.is(n2words('.1'), 'zero point one');
-  t.is(n2words(' -12.6 '), 'minus twelve point six');
-});
-
-test('error on unsupported languages', t => {
-  t.throws(
-    () => {
-      n2words(2, { lang: 'aaa' });
-    },
-    { instanceOf: Error }
-  );
-  t.throws(
-    () => {
-      n2words(2, 'en');
-    },
-    { instanceOf: Error }
-  );
-});
-
-test('error on invalid numbers', t => {
-  t.throws(
-    () => {
-      n2words('foobar');
-    },
-    { instanceOf: Error }
-  );
-  t.throws(
-    () => {
-      n2words(false);
-    },
-    { instanceOf: Error }
-  );
-  t.throws(
-    () => {
-      n2words('3px');
-    },
-    { instanceOf: Error }
-  );
-  t.throws(
-    () => {
-      n2words(NaN);
-    },
-    { instanceOf: Error }
-  );
-  t.throws(
-    () => {
-      n2words('');
-    },
-    { instanceOf: Error }
-  );
-  t.throws(
-    () => {
-      n2words(' ');
-    },
-    { instanceOf: Error }
-  );
-});
+}
