e88d21ccb56eb0c553c8fa9d5596b2c4c41dbd08
Tyler Vigario <TylerVigario90@gmail.com>
2023-05-13 22:44:28 -0700

    Initial implementation

---
+++ bench.mjs
@@ -0,0 +1,14 @@
+import Benchmark from 'benchmark';
+import n2words from './lib/i18n/EN.mjs';
+
+const suite = new Benchmark.Suite();
+
+suite
+  .add('9007199254740995', () => {
+    n2words(9007199254740991);
+  })
+  .on('cycle', event => {
+    // Output benchmark result by converting benchmark result to string
+    console.log(String(event.target));
+  })
+  .run();
