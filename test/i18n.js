022fefc6e4c9bb5b43dfada92fcebd295a751c70
Tyler Vigario <TylerVigario90@gmail.com>
2023-06-02 17:09:43 -0700

    Unfinished

--- test/i18n.js
+++ test/i18n.js
@@ -1,3 +1,5 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+/* eslint-disable import/no-nodejs-modules */
 // eslint-disable-next-line import/no-unresolved, node/no-missing-import
 import test from 'ava';
 
@@ -5,28 +7,23 @@ const parameter = process.argv[2];
 const value = process.argv[3];
 
 if (parameter == '--language' || parameter == '--lang') {
-  await testLanguage(value);
+  //testLanguage(value);
 } else {
   const {default: fs} = await import('fs/promises');
 
   const files = await fs.readdir('./lib/i18n');
 
   for (let i = 0; i < files.length; i++) {
-    await testLanguage(files[i].replace('.js', ''));
-  }
-}
+    const i18n = files[i].replace('.js', '');
+    const {default: language} = await import('../lib/i18n/' + i18n + '.js');
+    const {default: problems} = await import('./i18n/' + i18n + '.js');
 
-/**
- * Run i18n tests for specific language
- * @param {string} i18n language code to run
- */
-async function testLanguage(i18n) {
-  const {default: language} = await import('../lib/i18n/' + i18n + '.js');
-  const {default: problems} = await import('./i18n/' + i18n + '.js');
+    test(i18n, t => {
+      t.plan(problems.length);
 
-  for (let i = 0; i < problems.length; i++) {
-    test(i18n + ':' + problems[i][0], t => {
-      t.is(language(problems[i][0], problems[i][2]), problems[i][1]);
+      for (let i = 0; i < problems.length; i++) {
+        t.is(language(problems[i][0], problems[i][2]), problems[i][1]);
+      }
     });
   }
 }
