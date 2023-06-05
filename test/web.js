ef6002e9666fa6eb5c8b59f26b75bc97f3011896
Tyler Vigario <TylerVigario90@gmail.com>
2023-06-05 16:56:21 -0700

    Check for missing build file

--- test/web.js
+++ test/web.js
@@ -1,12 +1,19 @@
+/* eslint-disable import/no-nodejs-modules */
 // eslint-disable-next-line import/no-unresolved, node/no-missing-import
 import test from 'ava';
 import {Browser, Builder, By, until} from 'selenium-webdriver';
-// eslint-disable-next-line import/no-nodejs-modules
-import * as process from 'node:process';
 // eslint-disable-next-line import/no-unassigned-import
 import 'chromedriver';
+import * as process from 'node:process';
+import fs from 'fs';
 
-testBrowser(Browser.CHROME);
+const file = process.cwd() + '/test/web/index.html';
+
+if (!fs.existsSync(file)) {
+  throw new Error('You must run npm run build first.');
+} else {
+  testBrowser(Browser.CHROME);
+}
 
 /**
  * Test browser
@@ -14,9 +21,8 @@ testBrowser(Browser.CHROME);
  */
 async function testBrowser(browser) {
   test(browser, async t => {
-    const cwd = process.cwd();
     const driver = new Builder().forBrowser(browser).build();
-    await driver.get('file://' + cwd + '/test/web/index.html');
+    await driver.get('file://' + file);
     await driver.wait(until.elementTextContains(driver.findElement(By.css('#result')), 'Result:'));
     t.true(await (await driver.findElement(By.css('#result'))).getText() == 'Result: Success');
     await driver.close();
