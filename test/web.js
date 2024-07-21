85c6b3b1371c7babdd7f658e635ef4727c0f6149
Tyler Vigario <TylerVigario90@gmail.com>
2024-07-21 09:52:56 -0700

    Removed reliance on node:fs for wrapper

--- test/web.js
+++ test/web.js
@@ -2,37 +2,44 @@
 //// eslint-disable-next-line import/no-unresolved
 import test from 'ava';
 import { Browser, Builder, By, until } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome.js';
 //// eslint-disable-next-line import/no-unassigned-import
 import 'chromedriver';
 import { cwd } from 'node:process';
 
-await testBrowser(Browser.CHROME);
+const chromeOptions = new chrome.Options();
+chromeOptions.addArguments('--disable-web-security');
+
+await testBrowser(Browser.CHROME, chromeOptions);
 
 /**
  * Use browser to confirm working state of project.
  * @param {string} browser Browser object to use.
+ * @param {chrome.Options} options Chrome options
  * @see `Browser`
  */
-async function testBrowser(browser) {
+async function testBrowser(browser, options) {
   test(browser, async t => {
-    const options = new Chrome.Options();
     const driver = new Builder()
       .forBrowser(browser)
-      .setChromeOptions(options.addArguments('--disable-web-security'))
+      .setChromeOptions(options)
       .build();
 
     await driver.get(`file://${cwd()}/test/web/index.html`);
 
+    // Get <p id="result">...</p> element
+    const resultElement = await driver.findElement(By.css('#result'));
+
+    // Wait till inline script writes test result
     await driver.wait(
       until.elementTextContains(
-        driver.findElement(By.css('#result')),
+        resultElement,
         'Result:'
       )
     );
 
-    const result = await driver.findElement(By.css('#result'));
-
-    t.true((await result.getText()) == 'Result: Success');
+    // Check if inline test succeeded
+    t.true((await resultElement.getText()) == 'Result: Success');
 
     await driver.close();
   });
